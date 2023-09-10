3f72fd2ffa7094817a2667b28fe116382f6ba062
MohGharaibeh <gharaibehmoh3@gmail.com>
2023-09-10 18:45:52 +0300

    Version 1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=>import('./home/home.module').then(x=>x.HomeModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: ()=>import('./authentication/authentication.module').then(a=>a.AuthenticationModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: ()=> import('./admin/admin.module').then(a=>a.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { IndexComponent } from './index/index.component';
+import { SharedModule } from '../shared/shared.module';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    IndexComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule
+  ]
+})
+export class HomeModule { }
